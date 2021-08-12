dc3f2053cfeb768368c3ac0e322e6b3de6e88f33
reachify <60685625+Trtim@users.noreply.github.com>
2021-08-12 18:33:51 +0800

    Update autoheal.py

--- autoheal.py
+++ autoheal.py
@@ -2,8 +2,7 @@
 import time, keyboard
 
 autoheal = False
-chat = False
-d = 0.0001
+d = 0.1
 
 def press(a, b):
     keyboard.press(a)
@@ -12,16 +11,13 @@ def press(a, b):
     return
 
 while True:
-    if keyboard.is_pressed('enter'):
-        chat = not chat
-    if chat == False:
-        if keyboard.is_pressed('t'):
-            autoheal = not autoheal
-            while keyboard.is_pressed('t'):
-                    time.sleep(0)
-        if autoheal == True:
-                press('space', d)
-                press('3', d)
-                press('space', d)
-                press('1', d)
+    if keyboard.is_pressed('`'):
+        autoheal = not autoheal
+        while keyboard.is_pressed('`'):
+                time.sleep(0)
+    if autoheal == True:
+            press('space', d)
+            press('3', d)
+            press('space', d)
+            press('1', d)
 
